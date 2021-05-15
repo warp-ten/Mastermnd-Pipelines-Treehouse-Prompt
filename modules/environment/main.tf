aecc962c9f322f26b43173978a043938ca7233eb
xavier cordova <xaviercordovajr@gmail.com>
2021-05-15 11:54:56 -0500

    Add get IP and user_data to install nginx
    Add external data source to get my IP for use in security group. Added
    user_data to ec2 instance to install and run nginx for test purposes.
    Added security group ids because they were missing previously.

--- modules/environment/main.tf
+++ modules/environment/main.tf
@@ -8,13 +8,28 @@ data "aws_ssm_parameter" "linux2" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
+#Get my Public IP 
+data "external" "myipaddr" {
+  program = ["bash", "-c", "curl -s 'https://ipinfo.io/json'"]
+}
+
+
 resource "aws_instance" "ec2" {
-  count         = lookup(var.instance,"instance_count")
+  count         = lookup(var.instance, "instance_count")
   depends_on    = [aws_internet_gateway.gw]
   ami           = data.aws_ssm_parameter.linux2.value
-  instance_type = lookup(var.instance,"type")
-  subnet_id     = aws_subnet.subnet_1.id
+  instance_type = lookup(var.instance, "type")
+  subnet_id     = aws_subnet.subnet.id
+  # Commands to run on Instance at startup
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update
+    sudo yum install nginx -y
+    sudo systemctl enable nginx
+    sudo systemctl start nginx
+  EOF
   tags = {
     "Name" = "${var.env}-${count.index}"
   }
+  vpc_security_group_ids = [aws_security_group.sec_group.id]
 }
